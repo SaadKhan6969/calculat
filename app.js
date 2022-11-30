64df0182e56c03df975b30574b43c9681e843e06
saadkhan6969 <ayazsaad57@gmail.com>
2022-11-30 23:00:55 +0500

    Work Accomplish

---
+++ app.js
@@ -0,0 +1,12 @@
+var display=document.getElementById("display")
+function getvalue(a){
+    disp.value +=a
+}
+function bb(){
+var a=eval(disp.value)
+disp.value=a
+}
+
+function c(){
+    disp.value=""
+}
